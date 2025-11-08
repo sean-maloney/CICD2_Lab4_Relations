b4ab0bf02fb3f80e2f70650fd5fb9a24ae3bbb7a
sean-maloney <118452234+sean-maloney@users.noreply.github.com>
2025-11-08 23:51:35 +0000

    Added model going to add test case next

--- app/models.py
+++ app/models.py
@@ -1,11 +1,34 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Integer
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer, ForeignKey
+from typing import Optional 
+
+#User and Project database columns being mapped
+
 class Base(DeclarativeBase):
     pass
+
 class UserDB(Base):
     __tablename__ = "users"
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(String, nullable=False)
-    email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
     age: Mapped[int] = mapped_column(Integer, nullable=False)
-    student_id: Mapped[str]a = mapped_column(String, unique=True, nullable=False)
+    student_id: Mapped[str] = mapped_column(unique=True, nullable=False)
+    projects: Mapped[list["ProjectDB"]] = relationship(back_populates="owner", cascade="all,delete-orphan")
+
+class ProjectDB(Base):
+    __tablename__ = "projects"
+    project_id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[Optional[str]] = mapped_column(String, nullable=True)  # <- Optional
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    owner: Mapped["UserDB"] = relationship(back_populates="projects")
+
+
+# B) Independent table
+class CourseDB(Base):
+    __tablename__ = "courses"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    code: Mapped[str] = mapped_column(unique=True)
+    name: Mapped[str] = mapped_column(nullable=False) # required field
+    credits: Mapped[int] = mapped_column(nullable=False) # required field
