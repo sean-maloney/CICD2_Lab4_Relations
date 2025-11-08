75a042424d636990de046ba4dbe016028390f01e
sean-maloney <118452234+sean-maloney@users.noreply.github.com>
2025-11-08 23:49:32 +0000

    Finished setting up database.py, main.py and schemas.py, will add models next

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
-connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else{}
+
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
